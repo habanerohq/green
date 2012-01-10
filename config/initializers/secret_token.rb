db32bd18e904c0fc17c34934284a9a15f1cad318
Ben Caldwell <bencaldwell@einti.com>
2012-01-10 14:47:30 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sorbet2::Application.config.secret_token = 'fd368fa76ce02f183e83e7070a1f8481f4a387cc57dc7254a610fda15d0ec34d94071d31b9ce6c4ee4376223b09a3d278a82c99be62ad5f3b1d28594e0179efc'
