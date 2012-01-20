8e688a2bf81776e0ee43fa810ae0d12f7299cff7
Mark Ratjens <mark@ratjens.com>
2012-01-20 16:54:20 +1100

    extract DerivedIngredient from RelationIngredient
    and ingredient that does not directly create its own database columns should include this

--- app/ices/habanero/relation_ingredient_ice.rb
+++ app/ices/habanero/relation_ingredient_ice.rb
@@ -1,18 +1,7 @@
 module Habanero
   module RelationIngredientIce
     extend ActiveSupport::Concern
-
-    module InstanceMethods
-      protected
-
-      def add_columns
-      end
-
-      def change_columns
-      end
-
-      def remove_columns
-      end
-    end
+    
+    include DerivedIngredient
   end
 end
---
+++ lib/habanero/derived_ingredient.rb
@@ -0,0 +1,18 @@
+module Habanero
+  module DerivedIngredient
+    extend ActiveSupport::Concern
+
+    module InstanceMethods
+      protected
+
+      def add_columns
+      end
+
+      def change_columns
+      end
+
+      def remove_columns
+      end
+    end
+  end
+end
