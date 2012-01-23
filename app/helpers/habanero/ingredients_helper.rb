1707754db42684d5a8aba944e31bc03885fc7252
Mark Ratjens <mark@ratjens.com>
2012-01-23 18:43:17 +1100

    initial ingredients helpers

---
+++ app/helpers/habanero/ingredients_helper.rb
@@ -0,0 +1,48 @@
+module Habanero  
+  module IngredientsHelper
+    def format_habanero_currency_ingredient(target, ingredient)
+      if value = target.send(ingredient.qualified_name)
+        ingredient_span(ingredient) do
+          number_to_currency value, :precision => 2
+        end
+      end
+    end
+
+    def format_habanero_percentage_ingredient(target, ingredient)
+      if value = target.send(ingredient.method_name)
+        ingredient_span(ingredient) do
+          number_to_percentage value, :precision => 0
+        end
+      end
+    end
+
+    def format_habanero_range_ingredient(target, ingredient)
+      ingredient_span(ingredient) do
+        ingredient.children.map { |c| target.send(c.method_name) }.join('...')
+      end
+    end
+
+    def format_habanero_true_false_ingredient(target, ingredient)
+      value = "#{target.send(ingredient.method_name)}"
+      ingredient_span(ingredient, :class => value.idify) { value }
+    end
+
+    def format_ingredient(target, ingredient)
+      method = "format_#{ingredient.class.name.underscore}"
+      if respond_to?(method) && ingredient.class != Habanero::Ingredient
+        send(method, target, ingredient)
+      else
+        ingredient_span(ingredient) { "#{target.send(ingredient.qualified_name)}" }
+      end
+    end
+
+    def ingredient_span(ingredient, options = {}, &block)
+      options[:class] = "#{ingredient_css_classes(ingredient)} #{options[:class]}"
+      content_tag(:span, options, &block)
+    end
+
+    def ingredient_css_classes(ingredient)
+      "ingredient #{ingredient.class.to_s.idify} #{ingredient.qualified_name.idify}"
+    end
+  end
+end
--- lib/habanero/core_ext/string.rb
+++ lib/habanero/core_ext/string.rb
@@ -1,5 +1,9 @@
 class String
-  def attrify
-    titleize.gsub(/\s/, '').underscore
+  def attrify # an attribute identifier in the ruby idiom
+    titleize.gsub(/\s/, '').underscore.gsub('/', '_')
+  end
+  
+  def idify # an identitier in the script/css idiom
+    underscore.gsub(/\s|_/, '-')[/[A-Za-z0-9\-]*/]
   end
 end
