102dd9f9a7199fab5af79c6cd3ee1116b5deaa3e
Mark Ratjens <mark@ratjens.com>
2012-02-15 17:36:51 +1100

    get basic form text input working

--- app/helpers/habanero/ingredients_form_builder.rb
+++ app/helpers/habanero/ingredients_form_builder.rb
@@ -1,4 +1,20 @@
 module Habanero  
   class IngredientsFormBuilder < ActionView::Helpers::FormBuilder
+
+    def ingredient(i)
+      method = i.class.name.attrify
+      
+      if respond_to?(method) && i.class != Habanero::Ingredient
+        send(method, i)
+      else
+        @template.content_tag(:p) do
+          @template.content_tag(:label, i.name) <<
+          text_field(i.name.attrify)
+        end
+      end
+    end
+    
+    def habanero_nest_ingredient(i)
+    end
   end
 end
