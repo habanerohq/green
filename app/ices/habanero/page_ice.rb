d761f087cf8fefb38b35e5a4214687493ab2fa7d
Ben Caldwell <bencaldwell@einti.com>
2012-02-15 12:23:39 +0800

    Fix warning caused by some messy syntax

--- app/ices/habanero/page_ice.rb
+++ app/ices/habanero/page_ice.rb
@@ -9,13 +9,13 @@ module Habanero
     module InstanceMethods
       def draw_route(map, options = {})
         options[:constraints] = { :host => section.site.host } if section.site.host
-        map.match ({ qualified_path => 'habanero/pages#show', :defaults => { :draw_type => self.class.name, :draw_id => id } }).merge(options)
+        map.match({ qualified_path => 'habanero/pages#show', :defaults => { :draw_type => self.class.name, :draw_id => id } }).merge(options)
       end
-      
+
       def target_class
         nearest_target.try(:klass)
       end
-      
+
       def nearest_target
         target || section.nearest_target
       end
