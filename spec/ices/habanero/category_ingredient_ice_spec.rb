c5fd701f018cf6c01f92b6726bfccde2658fb6c4
Ben Caldwell <bencaldwell@einti.com>
2012-02-28 16:12:11 +0800

    Clean up some more tests so the work :v:

--- spec/ices/habanero/category_ingredient_ice_spec.rb
+++ spec/ices/habanero/category_ingredient_ice_spec.rb
@@ -1,23 +1,21 @@
 require 'spec_helper'
 
 describe Habanero::CategoryIngredient do
-  before(:each) do
+  before(:all) do
     @ingredient = Habanero::CategoryIngredient.new(:name => 'Foo Type')
-    
+
     @sorbet = Habanero::Sorbet.create!(
       :name => 'Dummy',
-      :parent => Habanero::Sorbet.first, # ActiveRecord::Base
-      :namespace => Habanero::Namespace.find_by_name('Habanero'),
-      :ingredients => [
-        Habanero::StringIngredient.new(:name => 'Foo'),
-        @ingredient
-      ]
+      :parent => Habanero::Sorbet.find_by_name('Base'),
+      :namespace => Habanero::Namespace.find_by_name('Habanero')
     )
+
+    @sorbet.ingredients << Habanero::StringIngredient.new(:name => 'Foo')
+    @sorbet.ingredients << @ingredient
   end
 
-  after(:each) do
-    @sorbet.destroy
-    ActiveSupport::Dependencies.clear
+  after(:all) do
+    Habanero::Sorbet.find_by_name('Dummy').try(:destroy)
   end
 
   it 'has a column name, method name and position name' do
@@ -28,7 +26,7 @@ describe Habanero::CategoryIngredient do
   it 'has a column type' do
     @ingredient.column_type.should == :integer
   end
-  
+
   it 'builds belongs_to association' do 
     a = Habanero::Dummy.reflect_on_association(:foo_type)
     a.should be_present
--- spec/ices/habanero/name_ingredient_ice_spec.rb
+++ spec/ices/habanero/name_ingredient_ice_spec.rb
@@ -1,17 +1,22 @@
 require 'spec_helper'
 
 describe Habanero::NameIngredient do
-  before do
+  before(:all) do
     @ingredient = Habanero::NameIngredient.new :name => 'Name', :format => '{{ name }}'
 
-    @namespace = Habanero::Namespace.find_by_name('Habanero')
-    @parent = Habanero::Sorbet.find_by_name('Base')
     @sorbet = Habanero::Sorbet.create!(
       :name => 'NameIngredientTestSorbet',
-      :parent => @parent,
-      :namespace => @namespace,
-      :ingredients => [@ingredient]
+      :parent => Habanero::Sorbet.find_by_name('Base'),
+      :namespace => Habanero::Namespace.find_by_name('Habanero')
     )
+
+    @sorbet.ingredients << @ingredient
+
+    @sorbet.chill!
+  end
+
+  after(:all) do
+    Habanero::Sorbet.find_by_name('NameIngredientTestSorbet').try(:destroy)
   end
 
   it 'should not allow more than one NameIngredient per Sorbet' do
@@ -26,8 +31,6 @@ describe Habanero::NameIngredient do
   end
 
   it 'should define a to_s method on the sorbet' do
-    @sorbet.chill!
-
     record = Habanero::NameIngredientTestSorbet.new :name => 'Foo'
     record.to_s.should == 'Foo'
   end
