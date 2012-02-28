2189e3a828838a15eed3c38caaa645a12d75d0c7
Ben Caldwell <bencaldwell@einti.com>
2012-02-28 16:59:00 +0800

    Fixed setting of default FormBuilder

--- config/initializers/sorbet.rb
+++ config/initializers/sorbet.rb
@@ -1,5 +1,4 @@
 require 'habanero'
 require 'habanero/autoload'
 
-# set the default form builder
-Sorbet2::Application.config.action_view.default_form_builder = Habanero::IngredientsFormBuilder
+ActionView::Base.default_form_builder = Habanero::IngredientsFormBuilder
