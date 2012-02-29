72fc6c51a4b31a6d4929d9bd2cc72ef3106be960
Ben Caldwell <bencaldwell@einti.com>
2012-02-29 23:28:41 +0800

    Upgrade to rails 3.2.1

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Sorbet2::Application.config.secret_token = 'fd368fa76ce02f183e83e7070a1f8481f4a387cc57dc7254a610fda15d0ec34d94071d31b9ce6c4ee4376223b09a3d278a82c99be62ad5f3b1d28594e0179efc'
+Sorbet2::Application.config.secret_token = '8251ceebf7823dbe7a16a9c3942c19c29c6dd3932f3bec4dcb49b9f960c5b9da60b09c1ba2e1ca7197f8d63c745792eaf2296cfb5e640e49982cd3670b829ef0'
