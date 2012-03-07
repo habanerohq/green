8ba3e68f8b4059b848b3d1333e53c55982088990
Ben Caldwell <bencaldwell@einti.com>
2012-03-07 14:55:08 +0800

    Fix db/schema.rb and prevent db:schema:dump from dumping tables generated by sorbets

--- db/schema.rb
+++ db/schema.rb
@@ -28,26 +28,6 @@ ActiveRecord::Schema.define(:version => 20120306055304) do
 
   add_index "cellar_migrations", ["version"], :name => "index_cellar_migrations_on_version", :unique => true
 
-  create_table "habanero_categories", :force => true do |t|
-    t.string  "abbreviation"
-    t.text    "strategy"
-    t.string  "name"
-    t.integer "parent_id"
-    t.integer "lft"
-    t.integer "rgt"
-    t.string  "slug"
-  end
-
-  add_index "habanero_categories", ["slug"], :name => "index_habanero_categories_on_slug", :unique => true
-
-  create_table "habanero_conditions", :force => true do |t|
-    t.string  "predicate"
-    t.string  "value"
-    t.integer "ingredient_id"
-    t.integer "query_id"
-    t.integer "query_position"
-  end
-
   create_table "habanero_ingredients", :force => true do |t|
     t.integer "sorbet_id"
     t.string  "name"
@@ -76,39 +56,6 @@ ActiveRecord::Schema.define(:version => 20120306055304) do
   add_index "habanero_ingredients", ["slug"], :name => "index_habanero_ingredients_on_slug"
   add_index "habanero_ingredients", ["sorbet_id"], :name => "index_habanero_ingredients_on_sorbet_id"
 
-  create_table "habanero_layout_rows", :force => true do |t|
-    t.boolean "fluid"
-    t.string  "name"
-    t.integer "layout_id"
-    t.integer "layout_position"
-  end
-
-  create_table "habanero_layouts", :force => true do |t|
-    t.string  "template_name"
-    t.boolean "fluid"
-    t.string  "name"
-    t.string  "slug"
-  end
-
-  add_index "habanero_layouts", ["slug"], :name => "index_habanero_layouts_on_slug", :unique => true
-
-  create_table "habanero_mask_ingredients", :force => true do |t|
-    t.integer "mask_id"
-    t.integer "mask_position"
-    t.integer "ingredient_id"
-  end
-
-  create_table "habanero_masks", :force => true do |t|
-    t.string  "name"
-    t.integer "parent_id"
-    t.integer "lft"
-    t.integer "rgt"
-    t.integer "sorbet_id"
-    t.string  "slug"
-  end
-
-  add_index "habanero_masks", ["slug"], :name => "index_habanero_masks_on_slug"
-
   create_table "habanero_namespaces", :force => true do |t|
     t.string "name"
     t.string "slug"
@@ -116,87 +63,6 @@ ActiveRecord::Schema.define(:version => 20120306055304) do
   end
 
   add_index "habanero_namespaces", ["slug"], :name => "index_habanero_namespaces_on_slug", :unique => true
-
-  create_table "habanero_pages", :force => true do |t|
-    t.string  "name"
-    t.integer "layout_id"
-    t.integer "template_id"
-    t.integer "section_id"
-    t.integer "section_position"
-    t.integer "target_id"
-    t.integer "next_page_id"
-    t.string  "slug"
-    t.string  "route"
-  end
-
-  add_index "habanero_pages", ["slug"], :name => "index_habanero_pages_on_slug"
-
-  create_table "habanero_queries", :force => true do |t|
-    t.string  "name"
-    t.integer "sorbet_id"
-  end
-
-  create_table "habanero_regions", :force => true do |t|
-    t.integer "span"
-    t.integer "offset"
-    t.string  "name"
-    t.integer "layout_id"
-    t.integer "row_id"
-    t.integer "row_position"
-    t.string  "slug"
-  end
-
-  add_index "habanero_regions", ["slug"], :name => "index_habanero_regions_on_slug"
-
-  create_table "habanero_scoop_placements", :force => true do |t|
-    t.string  "template"
-    t.integer "scoop_id"
-    t.integer "page_id"
-    t.integer "region_id"
-    t.integer "region_position"
-  end
-
-  create_table "habanero_scoops", :force => true do |t|
-    t.string  "type"
-    t.string  "title"
-    t.string  "body_format"
-    t.string  "template"
-    t.text    "body"
-    t.integer "columns"
-    t.string  "name"
-    t.integer "parent_id"
-    t.integer "lft"
-    t.integer "rgt"
-    t.integer "page_id"
-    t.integer "mask_id"
-    t.string  "slug"
-  end
-
-  add_index "habanero_scoops", ["slug"], :name => "index_habanero_scoops_on_slug", :unique => true
-
-  create_table "habanero_sections", :force => true do |t|
-    t.string  "name"
-    t.integer "parent_id"
-    t.integer "lft"
-    t.integer "rgt"
-    t.integer "site_id"
-    t.integer "site_position"
-    t.integer "template_id"
-    t.integer "target_id"
-    t.string  "slug"
-    t.string  "route"
-  end
-
-  add_index "habanero_sections", ["slug"], :name => "index_habanero_sections_on_slug"
-
-  create_table "habanero_sites", :force => true do |t|
-    t.string  "host"
-    t.string  "name"
-    t.integer "root_page_id"
-    t.string  "slug"
-  end
-
-  add_index "habanero_sites", ["slug"], :name => "index_habanero_sites_on_slug", :unique => true
 
   create_table "habanero_sorbets", :force => true do |t|
     t.integer "namespace_id"
