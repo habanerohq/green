ac73c0d33510f64eb85d241121b1542868d36e50
Ben Caldwell <bencaldwell@einti.com>
2012-03-07 08:55:29 +0800

    Fix some bootstrapping issues with observer

--- lib/habanero/observer.rb
+++ lib/habanero/observer.rb
@@ -4,7 +4,10 @@ class Habanero::Observer < Pantry::Observer
   # observe every sorbet
   if Habanero::Sorbet.table_exists?
     observe Habanero::Sorbet.all.map { |s| s.qualified_name.underscore.to_sym } - [:'active_record/base']
+    ::SorbetPantry.new.define_stacks
+  else
+    # todo: don't want to observe anything at this point?
+    observe :'habanero/sorbet'
   end
 
-  ::SorbetPantry.new.define_stacks
 end
