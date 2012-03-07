4403de1a7f9d3628d533dd1133e89429b0aff29c
Mark Ratjens <mark@ratjens.com>
2012-03-07 13:03:11 +1100

    use new Pantry::Observer protocol that requires an implementation of #define_stacks

--- lib/habanero/observer.rb
+++ lib/habanero/observer.rb
@@ -1,8 +1,9 @@
 require 'sorbet_pantry'
 
 class Habanero::Observer < Pantry::Observer
-  # observe every sorbet
   observe Habanero::Sorbet.all.map { |s| s.qualified_name.underscore.to_sym } - [:'active_record/base']
 
-  ::SorbetPantry.new.define_stacks
+  def define_stacks
+    ::SorbetPantry.new.define_stacks
+  end
 end
