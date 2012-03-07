b13db354871d428be47a2592ee6dca08b45d92a6
Ben Caldwell <bencaldwell@einti.com>
2012-03-07 09:12:56 +0800

    Some more fixes to bootstrapping for observer

--- config/initializers/sorbet.rb
+++ config/initializers/sorbet.rb
@@ -2,3 +2,8 @@ require 'habanero'
 require 'habanero/autoload'
 
 ActionView::Base.default_form_builder = Habanero::IngredientsFormBuilder
+
+if Habanero::Sorbet.table_exists?
+  ActiveRecord::Base.observers = :'habanero/observer'
+  ActiveRecord::Base.instantiate_observers
+end
