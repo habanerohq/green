91d4e84d6de9b8df39eb17296131eb28420666c7
Mark Ratjens <mark@ratjens.com>
2012-03-12 14:21:32 +1100

    scope current pantries

--- app/pantries/habanero_site_pantry.rb
+++ app/pantries/habanero_site_pantry.rb
@@ -13,24 +13,69 @@ class HabaneroSitePantry < Pantry::Base
     refers_to 'Habanero::Sorbet', :id_value_methods => [:name, :namespace]
     refers_to 'Habanero::Ingredient', :id_value_methods => [:name, :sorbet]
 
-    can_stack 'Habanero::Layout'
-    can_stack 'Habanero::LayoutRow', :id_value_methods => [:name, :layout]
-    can_stack 'Habanero::Region', :id_value_methods => [:name, :layout, :row]
+    can_stack 'Habanero::Layout', :scope => {:where => {:name => ['Habanero', 'Kitchen']}}
+    can_stack 'Habanero::LayoutRow', 
+      :id_value_methods => [:name, :layout], 
+      :scope => {
+        :includes => :layout,
+        :where => {:habanero_layouts => {:name => ['Habanero', 'ActiveRecord']}}
+      }
+    can_stack 'Habanero::Region', 
+      :id_value_methods => [:name, :layout, :row],
+      :scope => {
+        :includes => :layout,
+        :where => {:habanero_layouts => {:name => ['Habanero', 'ActiveRecord']}}
+      }
 
-    can_stack 'Habanero::Site'
-    can_stack 'Habanero::Section', :id_value_methods => [:name, :site, :parent]
-    can_stack 'Habanero::Page', :id_value_methods => [:name, :section]
+    can_stack 'Habanero::Site', :scope => {:where => {:name => 'Habanero'}}
+    can_stack 'Habanero::Section', 
+      :id_value_methods => [:name, :site, :parent],
+      :scope => {
+        :includes => :site,
+        :where => {:habanero_sites => {:name => 'Habanero'}}
+      }      
+    can_stack 'Habanero::Page', 
+      :id_value_methods => [:name, :section],
+      :scope => {
+        :includes =>  {:section => :site},
+        :where => {:habanero_sites => {:name => 'Habanero'}}
+      }      
+    can_stack 'Habanero::Mask', 
+      :id_value_methods => [:name, :sorbet],
+      :scope => {
+        :includes => {:sorbet => :namespace},
+        :where => {:habanero_namespaces => {:name => 'Habanero'}}
+      }
+      
+    can_stack 'Habanero::MaskIngredient', 
+      :id_value_methods => [:mask, :ingredient],
+      :scope => {
+        :includes => {:mask => {:sorbet => :namespace}},
+        :where => {:habanero_namespaces => {:name => 'Habanero'}}
+      }
     
-    can_stack 'Habanero::Mask', :id_value_methods => [:name, :sorbet]
-    can_stack 'Habanero::MaskIngredient', :id_value_methods => [:mask, :ingredient]
-    
-    can_stack 'Habanero::Query', :id_value_methods => [:name, :sorbet]
-    can_stack 'Habanero::Condition', :id_value_methods => [:query, :ingredient]
+    can_stack 'Habanero::Query', 
+      :id_value_methods => [:name, :sorbet],
+      :scope => {
+        :includes => {:sorbet => :namespace},
+        :where => {:habanero_namespaces => {:name => 'Habanero'}}
+      }
+    can_stack 'Habanero::Condition', 
+      :id_value_methods => [:query, :ingredient]
 
-    can_stack 'Habanero::Scoop'
-    can_stack 'Habanero::ScoopPlacement', :id_value_methods => [:template, :page, :scoop, :region]
+    can_stack 'Habanero::Scoop',
+      :id_value_methods => [:name, :namespace],
+      :scope => {
+        :includes => :namespace,
+        :where => {:habanero_namespaces => {:name => 'Habanero'}}
+      }
+    can_stack 'Habanero::ScoopPlacement', 
+      :id_value_methods => [:template, :page, :scoop, :region],
+      :scope => {
+        :includes => {:scoop => :namespace},
+        :where => {:habanero_namespaces => {:name => 'Habanero'}}
+      }
   end
-  
 end
 
 
--- app/pantries/sorbet_pantry.rb
+++ app/pantries/sorbet_pantry.rb
@@ -1,8 +1,13 @@
 class SorbetPantry < Pantry::Base
   
   def define_stacks
-    can_stack 'Habanero::Namespace'
-    can_stack 'Habanero::Sorbet', :id_value_methods => [:name, :namespace]
+    can_stack 'Habanero::Namespace', :scope => {:where => {:name => ['Habanero', 'ActiveRecord']}}
+    can_stack 'Habanero::Sorbet', 
+      :id_value_methods => [:name, :namespace], 
+      :scope => {
+        :includes => :namespace,
+        :where => {:habanero_namespaces => {:name => ['Habanero', 'ActiveRecord']}}
+      }
     
     [
       'Habanero::StringIngredient', 
@@ -25,7 +30,15 @@ class SorbetPantry < Pantry::Base
       'Habanero::SlugIngredient',
       'Habanero::CategoryIngredient',
       'Habanero::RouteIngredient'
-    ].each { |i| can_stack i, :id_value_methods => [:name, :sorbet, :parent] }
+    ].
+    each do |i| 
+      can_stack i, 
+      :id_value_methods => [:name, :sorbet, :parent],
+      :scope => {
+        :includes => {:sorbet => :namespace},
+        :where => {:habanero_namespaces => {:name => ['Habanero', 'ActiveRecord']}}
+      }
+    end
   end
 
 end
