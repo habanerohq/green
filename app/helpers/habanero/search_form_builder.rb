7283891e3f1ef8e6b4c228ccceb7d7d010d409d0
Mark Ratjens <mark@ratjens.com>
2012-03-13 13:45:57 +1100

    adapt nest and association ingredients helpers for searching

--- app/helpers/habanero/search_form_builder.rb
+++ app/helpers/habanero/search_form_builder.rb
@@ -1,5 +1,13 @@
 module Habanero
   class SearchFormBuilder < IngredientsFormBuilder
 
+    def habanero_nest_ingredient(i)
+      text_field :parent
+    end
+
+    def habanero_association_ingredient(i)
+      text_field i.method_name if i.relation == 'belongs_to'
+    end
+
   end
 end
