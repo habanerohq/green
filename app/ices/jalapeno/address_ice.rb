e34935f3ba1e3826a058f07893e70be3bd203312
Mark Ratjens <mark@ratjens.com>
2012-03-18 19:16:33 +1100

    start building sorbets for dreambridge components in jalapeno namespace

---
+++ app/ices/jalapeno/address_ice.rb
@@ -0,0 +1,29 @@
+module Jalapeno
+  module AddressIce
+    extend ActiveSupport::Concern
+    include GeoKit::Geocoders
+
+    included do
+      acts_as_mappable :default_units => :kms
+      before_validation :geocode
+    end
+
+    def geocode
+      geo = MultiGeocoder.geocode(to_s)
+      errors.add(:address, 'Could not Geocode address') if !geo.success
+      self.success = geo.success
+      self.provider = geo.provider
+      if geo.success
+        [:accuracy, :city, :country, :country_code, :full_address, :lat, :lng, :precision, :province, :state, :zip].each do |method|
+          self.send("#{method}=", geo.send(method)) unless geo.send(method).blank?
+        end
+      end
+    end
+
+    module ClassMethods
+      def distance_to_zoom(kilometers)
+        14 - Math.log2(kilometers.to_f).floor if kilometers
+      end
+    end
+  end
+end
---
+++ app/pantries/jalapeno_pantry.rb
@@ -0,0 +1,43 @@
+class JalapenoPantry < Pantry::Base
+  
+  def define_stacks
+    can_stack 'Habanero::Namespace', :scope => {:where => {:name => 'Jalapeno'}}
+    can_stack 'Habanero::Sorbet', 
+      :id_value_methods => [:name, :namespace], 
+      :scope => {
+        :includes => :namespace,
+        :where => {:habanero_namespaces => {:name => 'Jalapeno'}}
+      }
+    
+    [
+      'Habanero::StringIngredient', 
+      'Habanero::TextIngredient', 
+      'Habanero::BlobIngredient', 
+      'Habanero::TrueFalseIngredient', 
+      'Habanero::IntegerIngredient', 
+      'Habanero::DecimalIngredient', 
+      'Habanero::NumberIngredient', 
+      'Habanero::PercentageIngredient', 
+      'Habanero::CurrencyIngredient', 
+      'Habanero::DateIngredient', 
+      'Habanero::DateTimeIngredient', 
+      'Habanero::TimeIngredient', 
+      'Habanero::NameIngredient', 
+      'Habanero::RangeIngredient', 
+      'Habanero::NestIngredient', 
+      'Habanero::RelationIngredient', 
+      'Habanero::AssociationIngredient', 
+      'Habanero::SlugIngredient',
+      'Habanero::CategoryIngredient',
+      'Habanero::RouteIngredient'
+    ].
+    each do |i| 
+      can_stack i, 
+      :id_value_methods => [:name, :sorbet, :parent],
+      :scope => {
+        :includes => {:sorbet => :namespace},
+        :where => {:habanero_namespaces => {:name => 'Jalapeno'}}
+      }
+    end
+  end
+end
