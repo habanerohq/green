45f7554032b6629f9a6e56f668816aa72b4817cd
Mark Ratjens <mark@ratjens.com>
2012-03-22 18:30:17 +1100

    allow new objects to be created from connections template

--- app/helpers/habanero/pages_helper.rb
+++ app/helpers/habanero/pages_helper.rb
@@ -4,5 +4,9 @@ module Habanero
       send "page_#{page.id}_path", params
       #url_for(params.merge :use_route => "page_#{page.id}")
     end
+    
+    def new_sorbet_page
+      Habanero::ScoopPlacement.joins(:scoop, :page).where(:template => 'new', :habanero_scoops => {:type => 'Habanero::SorbetScoop'}).first.page
+    end
   end
 end
