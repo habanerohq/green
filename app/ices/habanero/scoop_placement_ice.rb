3a6cd42079e2632b566de8187c2d4d7cbe7e4926
Ben Caldwell <bencaldwell@einti.com>
2012-03-23 07:18:16 +0800

    Fix an issue with default tempalte for ScoopPlacement

--- app/ices/habanero/scoop_placement_ice.rb
+++ app/ices/habanero/scoop_placement_ice.rb
@@ -9,27 +9,27 @@ module Habanero
     def query
       scoop.query
     end
-    
+
     def search
       scoop.search
     end
-    
+
     def ingredients
       scoop.ingredients
     end
-    
-    def prepare_search(params, page)
+
+   def prepare_search(params, page)
       if data = params[params_key]
         scoop.translate_search(data)
         page.placements.each { |p| p.scoop.search = scoop.search if p.scoop.id == scoop.id }
       end
     end
-    
+
     def prepare(*args)
-      method = "prepare_#{template.attrify}"
+      method = "prepare_#{template.try(:attrify) || 'show'}"
       send(method, *args) if respond_to?(method)
     end
-    
+
     def params_key
       "#{template}_#{id}"
     end
