e1e07232788eb655549b4a71e32e03a76c40f1cd
Mark Ratjens <mark@ratjens.com>
2012-03-30 17:10:44 +1100

    get table column sorting to work with tables that depend on a default set-up

--- app/cells/habanero/sorbet_collection_cell.rb
+++ app/cells/habanero/sorbet_collection_cell.rb
@@ -15,16 +15,18 @@ module Habanero
       if data = params[@placement.params_key]
         @targets = data.reduce(@targets) do |result, (method, direction)|
 
-          if @query.klass.column_names.include?(method)
+          if klass_by_precedence.column_names.include?(method)
             result.order("#{method} #{direction}")
 
           else
-            k = @query.klass.reflect_on_association(method.to_sym).klass
+            k = klass_by_precedence.reflect_on_association(method.to_sym).klass
             tn = k.table_name
             cn = (k.column_names.include?('name') ? 'name' : 'id') # todo: what to do if name is not the same as to_s
             result.includes(method).order("#{tn}.#{cn} #{direction}")
           end
         end
+      else
+        @targets = @targets.order(@sorbet.klass.to_s_methods)
       end
       render
     end
@@ -58,12 +60,16 @@ module Habanero
       end
     end
     
+    def klass_by_precedence
+      @query.try(:klass) || @sorbet.try(:klass)
+    end
+    
     def sorbet_by_precedence
       @query.try(:sorbet) || @mask.try(:sorbet) || @page.nearest_target
     end
     
     def targets_by_precedence
-      @search.try(:query_chain) || @query.try(:evaluate, params) || (@sorbet.klass.order(@sorbet.klass.to_s_methods) if @sorbet.present?) || []
+      @search.try(:query_chain) || @query.try(:evaluate, params) || (@sorbet.klass.unscoped if @sorbet.present?) || []
     end
     
     def ingredients_by_precedence
