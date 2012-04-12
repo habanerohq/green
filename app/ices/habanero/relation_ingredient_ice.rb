1b704fbf9b389f119035336e35dbad726053b090
Mark Ratjens <mark@ratjens.com>
2012-04-12 12:14:53 +1000

    refine name derivation for default relation ingredients

--- app/ices/habanero/relation_ingredient_ice.rb
+++ app/ices/habanero/relation_ingredient_ice.rb
@@ -12,13 +12,16 @@ module Habanero
     protected
     
     def derive_name
-      self.name = "#{name} #{associated_name}" if respond_to?(:associated_name)
+      if respond_to?(:associated_name)
+        an = associated_name.try(:downcase)
+        self.name = "#{name} #{an}" unless an.blank? or name.include?(an)
+      end
     end
 
     def create_associations
       if respond_to?(:associated_name) and respond_to?(:associated_type) and associated_type.present?
         Habanero::AssociationIngredient.create(:parent => self, :name => self.associated_name, :relation => 'has_many', :sorbet => self.sorbet)
-        Habanero::AssociationIngredient.create(:parent => self, :name => self.name.gsub(" #{associated_name}", ''), :relation => 'belongs_to', :sorbet => self.associated_type)
+        Habanero::AssociationIngredient.create(:parent => self, :name => self.name.gsub(" #{associated_name.downcase}", ''), :relation => 'belongs_to', :sorbet => self.associated_type)
       end
     end
   end
