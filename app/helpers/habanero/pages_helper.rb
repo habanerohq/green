155710c9a0204e629090f5b996feb47d82ae9075
Mark Ratjens <mark@ratjens.com>
2012-04-16 17:06:04 +1000

    allow PageCollectionScoop#navigation to also work with objects that implement #page
    not sure if this is good design or a convenient hack … designed to allow sections to be used in page collection navigation … much easier than mucking around with a complex query or building a SectionCollectionScoop which would not be very DRY

--- app/helpers/habanero/pages_helper.rb
+++ app/helpers/habanero/pages_helper.rb
@@ -8,5 +8,16 @@ module Habanero
     def new_sorbet_page
       Habanero::ScoopPlacement.joins(:scoop, :page).where(:template => 'new', :habanero_scoops => {:type => 'Habanero::SorbetScoop'}).first.page
     end
+    
+    def target_pages(targets)
+      targets.map do |t|
+        case
+        when t._sorbet.name == 'Page'
+          t
+        when t.respond_to?(:page)
+          t.page
+        end
+      end.compact
+    end
   end
 end
