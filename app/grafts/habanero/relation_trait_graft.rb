9c0f2a64164554259a42ff63dfcbbbfb14414bad
Mark Ratjens <mark@ratjens.com>
2012-04-26 20:47:22 +1000

    give up on trying to be smart with naming relation traits

--- app/grafts/habanero/relation_trait_graft.rb
+++ app/grafts/habanero/relation_trait_graft.rb
@@ -5,23 +5,15 @@ module Habanero
     include DerivedTrait
 
     included do
-      before_create :derive_name
       after_create :create_associations
     end
 
     protected
-    
-    def derive_name
-      if respond_to?(:associated_name)
-        an = associated_name.try(:downcase)
-        self.name = "#{name} #{an}" unless an.blank? or name.include?(an)
-      end
-    end
 
     def create_associations
       if respond_to?(:associated_name) and respond_to?(:associated_type) and associated_type.present?
         Habanero::AssociationTrait.create(:parent => self, :name => self.associated_name, :relation => 'has_many', :variety => self.variety)
-        Habanero::AssociationTrait.create(:parent => self, :name => self.name.gsub(" #{associated_name.downcase}", ''), :relation => 'belongs_to', :variety => self.associated_type)
+        Habanero::AssociationTrait.create(:parent => self, :name => self.name, :relation => 'belongs_to', :variety => self.associated_type)
       end
     end
   end
