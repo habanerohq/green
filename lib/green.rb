bd4d0fb2389d12e2f6b238769b82332a3fafc040
Mark Ratjens <mark@ratjens.com>
2012-05-11 17:24:03 +1000

    manually require some more gems :(

--- lib/green.rb
+++ lib/green.rb
@@ -4,6 +4,8 @@ require 'cells'
 require 'friendly_id'
 require 'kaminari'
 require 'simple_form'
+require 'acts-as-taggable-on'
+require 'less-rails-bootstrap'
 require 'green/engine'
 
 module Green
