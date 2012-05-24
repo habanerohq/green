469c24e6e6457eecb78439bfaebdc3860337332a
Mark Ratjens <mark@ratjens.com>
2012-05-24 17:00:24 +1000

    when a new site is created, also create a basic minimal structure for a root_page to be ready for placing features.

--- app/grafts/habanero/site_graft.rb
+++ app/grafts/habanero/site_graft.rb
@@ -2,10 +2,30 @@ module Habanero
   module SiteGraft
     extend ActiveSupport::Concern
 
+    included do
+      before_create :create_default_initial_objects
+    end
+
     module ClassMethods
       def current
         @current ||= self.first # need to be able to configure this!
       end
+    end
+    
+    def create_default_initial_objects
+      Habanero::Brand.create(:name => name)
+
+      g = Habanero::Garden.create(
+        :name => "#{name} front",
+        :signpost => '/front'
+      )
+
+      self.root_scene = Habanero::Scene.create(
+        :name => 'Index',
+        :signpost => '/',
+        :garden => g,
+        :layout => Habanero::Layout.find_by_name('Habanero')
+      )
     end
   end
 end
