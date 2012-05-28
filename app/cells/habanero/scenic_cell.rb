ef581849732b98c33d39a5dfd82486004ce69495
Mark Ratjens <mark@ratjens.com>
2012-05-28 22:20:32 +1000

    make ScenicCell#plan go now … even when a scene has no layout defined

--- app/cells/habanero/scenic_cell.rb
+++ app/cells/habanero/scenic_cell.rb
@@ -5,6 +5,7 @@ module Habanero
     def plan(options)
       get_started(options)
       @target = find_target
+      @layout = @target.nearest_layout
       @placements = @target.all_placements
       render
     end
